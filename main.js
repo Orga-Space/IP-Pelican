debbe92e5bffc4af4f57db8fff967e0f6ac3a961
orga <orga.space@posteo.org>
2021-04-25 14:45:36 +0200

    added web scraper for ip address

---
+++ main.js
@@ -0,0 +1,87 @@
+const https = require('https')
+const fs = require('fs')
+
+const regex = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/gm
+
+const ipPageUrl = '';
+
+(async function main() {
+  try {
+    const webPage = await getWebContend(ipPageUrl)
+    const ip = getIpFromPage(webPage)
+    if (await didIpChanged(ip)) {
+      // callDDNS service
+      updateIpLogs(ip)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})()
+
+
+function getWebContend(page) {
+  return new Promise((resolve, reject) => {
+    https.get(page, (res) => {
+      res.setEncoding('utf-8')
+      let rawData = ""
+
+      res.on('data', (chunk) => rawData += chunk)
+
+      res.on('end', () => resolve(rawData))
+    }).on('error', (e) => reject(e))
+  })
+}
+
+function getIpFromPage(webPage) {
+  const potentialDuplicatedIpAddresses = webPage.match(regex)
+  const uniqueIpAddresses = Array.from(new Set(potentialDuplicatedIpAddresses))
+  
+  if (uniqueIpAddresses.length === 0) throw new Error("No IP Address found")
+  if (uniqueIpAddresses.length > 1) throw new Error("More than one IP Addres was found; IP Address Array: " + uniqueIpAddresses)
+  return uniqueIpAddresses[0]
+}
+
+async function didIpChanged(ip) {
+  const lastIp = await readIpFromFile()
+  console.log(lastIp)
+  console.log(ip)
+
+  if (lastIp === ip) return false
+  return true
+}
+
+function readIpFromFile() {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(__dirname + '/iplogs/currentIp', 'utf8', (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+function updateIpLogs(ip) {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(__dirname + '/iplogs/log.csv', `${getFormatedDate()}, ${ip}\n`, (err) => {
+      if (err) reject(err)
+    })
+    fs.writeFile(__dirname + '/iplogs/currentIp', ip, (err) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
+
+function getFormatedDate() {
+  let current = new Date();
+  let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+  let cTime = current.getHours() + ":" + current.getMinutes();
+  let dateTime = cDate + ' ' + cTime;
+  return dateTime
+}
