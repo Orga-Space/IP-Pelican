82302ecdc6d1ddd6bdb3b3e4f4faf9a5bb31509a
orga <orga.space@posteo.org>
2021-08-13 20:30:18 +0200

    WIP: expanded API

--- src/server/apiServer.js
+++ src/server/apiServer.js
@@ -5,19 +5,48 @@ const fs = require('fs');
 /**
  * Starts the API for the logs.
  * @param {Number} port 
- * @param {String} logFilePath 
+ * @param {Object} paths 
  */
-function startAPI(port, logFilePath) {
+function startAPI(port, paths) {
     http.createServer((req, res) => {
-        fs.readFile(logFilePath, (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.end(JSON.stringify(err));
-            } else {
-                res.writeHead(200);
-                res.end(data);
-            }
-        });
+        switch(req.url) {
+            case '/api/currentIP':
+                fs.readFile(paths.currentIpFile, (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end(JSON.stringify(err));
+                    } else {
+                        res.writeHead(200);
+                        res.end(data);
+                    }
+                });
+                break;
+            case '/api/updatelog':
+                fs.readFile(paths.ipUpdateLog, (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end(JSON.stringify(err));
+                    } else {
+                        res.writeHead(200);
+                        res.end(data);
+                    }
+                });
+                break;
+            case '/api/ddnssResponseLog':
+                fs.readFile(paths.ddnssResponseLogFile, (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end(JSON.stringify(err));
+                    } else {
+                        res.writeHead(200);
+                        res.end(data);
+                    }
+                });
+                break;
+            default:
+                res.writeHead(404);
+                res.end(`Path: ${req.url} does not exist.`)
+        }
     }).listen(port);
 }
 
